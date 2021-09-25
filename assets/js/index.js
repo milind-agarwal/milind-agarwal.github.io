b950dc28d9709ce837906ac976ef44f043a6172d
milind-agarwal <milind.waterbender@gmail.com>
2021-09-25 12:39:23 +0530

    added logic to make sure my profile photo resizes

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,6 @@ list_of_names[parseInt(display_name) - 1].style.display = 'block'
 var intervalId = window.setInterval(function(){
   list_of_names = $('.my_name').children()
   let display_name = sessionStorage.getItem("display_name");
-  console.log(display_name)
   if (display_name != 0) {
     list_of_names[display_name - 1].style.display = 'none'
   }
