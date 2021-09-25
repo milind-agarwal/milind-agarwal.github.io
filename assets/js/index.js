8add6e3bdc1b1a6d1d578e261377fe043f235213
milind-agarwal <milind.waterbender@gmail.com>
2021-09-25 12:18:31 +0530

    transition carries to different subpages, also updated Bio

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,12 @@
-display_name = 1
+
+list_of_names = $('.my_name').children()
+let display_name = sessionStorage.getItem("display_name");
+list_of_names[parseInt(display_name) - 1].style.display = 'block'
+
 var intervalId = window.setInterval(function(){
   list_of_names = $('.my_name').children()
+  let display_name = sessionStorage.getItem("display_name");
+  console.log(display_name)
   if (display_name != 0) {
     list_of_names[display_name - 1].style.display = 'none'
   }
@@ -8,5 +14,6 @@ var intervalId = window.setInterval(function(){
     display_name = 0
   }
   list_of_names[display_name].style.display = 'block'
-  display_name = display_name + 1
+  sessionStorage.setItem("display_name",parseInt(display_name) + 1);
+
 }, 2000);
