f3b3326146bf2fd6f815958677dac1cec74ad803
milind-agarwal <milind.waterbender@gmail.com>
2022-07-03 21:59:11 -0400

    redid the dropdown. now it moves with the button

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,3 +16,37 @@
 //   sessionStorage.setItem("display_name",parseInt(display_name) + 1);
 //
 // }, 2000);
+
+
+// document.addEventListener('click', function(event) {
+//   var specifiedElement = $("#site-nav > ul > button")[0];
+//
+//   var isClickInside = specifiedElement.contains(event.target);
+//   if (isClickInside) {
+//     // alert('You clicked inside A');
+//     $("#site-nav > ul > div")[0].style.visibility = 'visible';
+//   } else {
+//     // alert('You clicked outsi A');
+//     $("#site-nav > ul > div")[0].style.visibility = 'hidden';
+//   }
+// });
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
