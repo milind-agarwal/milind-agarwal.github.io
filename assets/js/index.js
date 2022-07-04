7dd667e3209c2d58c6060f815be72f5194ee9772
milind-agarwal <milind.waterbender@gmail.com>
2022-07-03 20:18:27 -0400

    removed JS for changing name

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,18 @@
-
-list_of_names = $('.my_name').children()
-let display_name = sessionStorage.getItem("display_name");
-list_of_names[parseInt(display_name) - 1].style.display = 'block'
-
-var intervalId = window.setInterval(function(){
-  list_of_names = $('.my_name').children()
-  let display_name = sessionStorage.getItem("display_name");
-  if (display_name != 0) {
-    list_of_names[display_name - 1].style.display = 'none'
-  }
-  if (display_name == list_of_names.length) {
-    display_name = 0
-  }
-  list_of_names[display_name].style.display = 'block'
-  sessionStorage.setItem("display_name",parseInt(display_name) + 1);
-
-}, 2000);
+//
+// list_of_names = $('.my_name').children()
+// let display_name = sessionStorage.getItem("display_name");
+// list_of_names[parseInt(display_name) - 1].style.display = 'block'
+//
+// var intervalId = window.setInterval(function(){
+//   list_of_names = $('.my_name').children()
+//   let display_name = sessionStorage.getItem("display_name");
+//   if (display_name != 0) {
+//     list_of_names[display_name - 1].style.display = 'none'
+//   }
+//   if (display_name == list_of_names.length) {
+//     display_name = 0
+//   }
+//   list_of_names[display_name].style.display = 'block'
+//   sessionStorage.setItem("display_name",parseInt(display_name) + 1);
+//
+// }, 2000);
