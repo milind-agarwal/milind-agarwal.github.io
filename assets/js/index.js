044cc608778bee82c32067812dd9dbcee5457709
milind-agarwal <milind.waterbender@gmail.com>
2024-06-01 17:05:46 +0530

    added dark mode

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,41 +1,28 @@
-//
-// list_of_names = $('.my_name').children()
-// let display_name = sessionStorage.getItem("display_name");
-// list_of_names[parseInt(display_name) - 1].style.display = 'block'
-//
-// var intervalId = window.setInterval(function(){
-//   list_of_names = $('.my_name').children()
-//   let display_name = sessionStorage.getItem("display_name");
-//   if (display_name != 0) {
-//     list_of_names[display_name - 1].style.display = 'none'
-//   }
-//   if (display_name == list_of_names.length) {
-//     display_name = 0
-//   }
-//   list_of_names[display_name].style.display = 'block'
-//   sessionStorage.setItem("display_name",parseInt(display_name) + 1);
-//
-// }, 2000);
 
 
-// document.addEventListener('click', function(event) {
-//   var specifiedElement = $("#site-nav > ul > button")[0];
-//
-//   var isClickInside = specifiedElement.contains(event.target);
-//   if (isClickInside) {
-//     // alert('You clicked inside A');
-//     $("#site-nav > ul > div")[0].style.visibility = 'visible';
-//   } else {
-//     // alert('You clicked outsi A');
-//     $("#site-nav > ul > div")[0].style.visibility = 'hidden';
-//   }
-// });
+$(document).on('ready', function() {
+  modepersist();
+})
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+
+function modepersist() {
+  if (window.localStorage["globalMode"] == 'dark-mode') {
+    dark_mode()
+  }
+  else {
+    light_mode()
+  }
 }
+
+function dark_mode_background() {
+  document.documentElement.style.setProperty('--body_background_color', 'rgb(29, 30, 32)');
+		document.documentElement.style.setProperty('--body_text_color', 'rgb(218, 218, 219)');
+}
+function myFunction() {
+  classList = $("#myDropdown")[0].classList;
+  classList.toggle("show");
+}
+
 function myFunction2() {
   document.getElementById("myDropdown2").classList.toggle("show");
 }
@@ -53,10 +40,79 @@ window.onclick = function(event) {
       }
     }
   }
+
 }
 
 
-$("a.lateef").click(function(){
-   // var element = $("div");
-   // element[2].style.fontFamily = "Lateef", cursive;
+// $("a.lateef").click(function(){
+//    // var element = $("div");
+//    // element[2].style.fontFamily = "Lateef", cursive;
+// });
+
+
+function dark_mode() {
+  window.localStorage["globalMode"] =  "dark-mode";
+  // Action to change the display settings
+
+  dark_mode_background()
+  // $("body")[0].style.backgroundColor = 
+  // $("body")[0].style.color = 
+
+  anchors = $("a")
+  for (let i = 0; i < anchors.length; i++) {
+    anchors[i].classList.add('dark-mode')
+  }
+
+  buttons = $("button")
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.add('dark-mode')
+  }
+
+  document.getElementById("myDropdown").classList.add('dark-mode')
+
+  // Action to change the logo again 
+
+  $("#moon-anchor")[0].style.display = 'none'
+  $("#sun-anchor")[0].style.display = 'inline'
+  
+
+}
+
+function light_mode() {
+  window.localStorage["globalMode"] = ""
+  // Action to change the display settings
+  document.documentElement.style.setProperty('--body_background_color', '#fff' );
+  document.documentElement.style.setProperty('--body_text_color', '#333332');
+
+  // $("body")[0].style.backgroundColor = 
+  // $("body")[0].style.color = 
+
+  anchors = $("a")
+  for (let i = 0; i < anchors.length; i++) {
+    anchors[i].classList.remove('dark-mode')
+  }
+
+  buttons = $("button")
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove('dark-mode')
+  }
+
+  document.getElementById("myDropdown").classList.remove('dark-mode')
+
+  // Action to change the logo again 
+
+  $("#moon-anchor")[0].style.display = 'inline'
+  $("#sun-anchor")[0].style.display = 'none'
+
+ 
+}
+
+$("a#moon-anchor").click(function(){
+  dark_mode();
 });
+
+$("a#sun-anchor").click(function(){
+  light_mode();
+
+});
+
